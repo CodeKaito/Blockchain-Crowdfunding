2a5f77e40d1d7c1722b39f5d10ce9e1b5247e1b2
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-09-15 09:57:23 +0200

    modified:   client/src/components/Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,8 +5,11 @@ import { logo, sun } from '../assets';
 import {navlinks } from '../constants';
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
-  <div>
-    
+  <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`}>
+      {!isActive 
+        ? (<img src={imgUrl} alt="fund_logo" className='w-1/2 h-1/2' />)
+        : (<img src={imgUrl} alt="fund_logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />)
+      } 
   </div>
 )
 
@@ -24,4 +27,4 @@ const Sidebar = () => {
   )
 }
 
-export default Sidebar
+export default Sidebar;
