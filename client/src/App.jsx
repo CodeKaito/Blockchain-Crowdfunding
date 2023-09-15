74af569e3831cc8f3a0b7f1106a4ec7e7cd231f1
lightmnd <booking.djonly@gmail.com>
2023-09-15 14:33:05 +0200

    fix: fixed routing configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,21 @@
-
-import React from 'react';
-import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
-import { Home, Profile, CampaignDetails, CreateCampaign } from './pages';
-import Sidebar from './components/Sidebar';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+import { Home, Profile, CampaignDetails, CreateCampaign } from "./pages";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row '>
-      <div className='sm:flex hidden mr-10'>
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row ">
+      <div className="sm:flex hidden mr-10">
         <Sidebar />
       </div>
 
-      <div className='flex-1 max-sm:w-full max-w-[1280px] max-auto sm:pr-5'>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] max-auto sm:pr-5">
         <Navbar />
-
-        <BrowserRouter>
-          <Router>
-            <Routes>
-              <Route path='/' element={<Home />} />
-            </Routes>
-          </Router>
-        </BrowserRouter>
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </div>
   );
