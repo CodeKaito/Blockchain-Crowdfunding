ca889e0557ebf0cdafc1a60c4e95672a567406b3
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-09-17 19:22:21 +0200

    modified:   client/src/components/CustomButton.jsx

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const CustomButton = () => {
+const CustomButton = ({ btnType, title, handleClick, styles }) => {
   return (
-    <div>CustomButton</div>
+    <button 
+      type={btnType}
+      className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] rounded-[10px] ${styles}`}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
   )
 }
 
