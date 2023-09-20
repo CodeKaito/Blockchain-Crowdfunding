051ad34e5f95344bc33e226920f7ec4f144b2cb3
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-09-20 17:24:37 +0200

    new file:   client/src/components/Loader.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 
+import { useStateContext } from '../context';
 import { CustomButton } from './';
 import { logo, menu, search, thirdweb } from '../assets';
 import { navlinks } from '../constants';
@@ -12,7 +13,7 @@ const Navbar = () => {
 
   const [toggleDrawer, setToggleDrawer] = useState(false);
 
-  const address = '';
+  const { connect, address } = useStateContext();
 
   return (
     <div className='flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6'>
@@ -30,7 +31,7 @@ const Navbar = () => {
           styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
           handleClick={() => {
             if(address) navigate('create-campaign')
-            else 'connect()';
+            else connect();
           }}
         />
 
@@ -82,7 +83,7 @@ const Navbar = () => {
                               styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
                               handleClick={() => {
                                 if(address) navigate('create-campaign')
-                                else 'connect()';
+                                else connect();
                               }}
                             />
                           </div>
--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -7,40 +7,95 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-    const { contract } = useContract('0x2A9195D235F99C0314bE79BdbDac36dA212931f3');
-    const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
+  const { contract } = useContract('0x0225891111f504A9eacCAa557b8952d7F0148A40');
+  const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
-    const address = useAddress();
-    const connect = useMetamask();
+  const address = useAddress();
+  const connect = useMetamask();
 
-    const publishCampaign = async (form) => {
-        try {
-            const data = await createCampaign([
-                address, // owner
-                form.title, // title
-                form.description, // description
-                form.target, // target
-                new Date(form.deadline).getTime(), // deadline
-                form.image, // image
-            ])
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign({
+				args: [
+					address, // owner
+					form.title, // title
+					form.description, // description
+					form.target,
+					new Date(form.deadline).getTime(), // deadline,
+					form.image,
+				],
+			});
 
-            console.log("contract call success", data);
-        } catch (error) {
-            console.log("contract call failure", error);
-        }
+      console.log("contract call success", data)
+    } catch (error) {
+      console.log("contract call failure", error)
+    }
+  }
+
+  const getCampaigns = async () => {
+    const campaigns = await contract.call('getCampaigns');
+
+    const parsedCampaings = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+      image: campaign.image,
+      pId: i
+    }));
+
+    return parsedCampaings;
+  }
+
+  const getUserCampaigns = async () => {
+    const allCampaigns = await getCampaigns();
+
+    const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+
+    return filteredCampaigns;
+  }
+
+  const donate = async (pId, amount) => {
+    const data = await contract.call('donateToCampaign', [pId], { value: ethers.utils.parseEther(amount)});
+
+    return data;
+  }
+
+  const getDonations = async (pId) => {
+    const donations = await contract.call('getDonators', [pId]);
+    const numberOfDonations = donations[0].length;
+
+    const parsedDonations = [];
+
+    for(let i = 0; i < numberOfDonations; i++) {
+      parsedDonations.push({
+        donator: donations[0][i],
+        donation: ethers.utils.formatEther(donations[1][i].toString())
+      })
     }
 
-    return (
-        <StateContext.Provider
-            value={{
-                address,
-                contract,
-                createCampaign: publishCampaign,
-            }}
-        >
-            {children}
-        </StateContext.Provider>
-    )
+    return parsedDonations;
+  }
+
+
+  return (
+    <StateContext.Provider
+      value={{ 
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+        getCampaigns,
+        getUserCampaigns,
+        donate,
+        getDonations
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  )
 }
 
 export const useStateContext = () => useContext(StateContext);
